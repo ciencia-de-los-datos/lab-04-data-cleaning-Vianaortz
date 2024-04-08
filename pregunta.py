90f2e2278e17cb85027b4c34aebc29874b5144b0
Vianaortz <levortizpe@unal.edu.co>
2024-04-08 14:08:47 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -13,8 +13,42 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+    df = df.drop('Unnamed: 0', axis=1)
+
+    df['sexo'] = df['sexo'].str.lower()
+
+
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
+
+    df['idea_negocio'] = df['idea_negocio'].str.lower()
+    df['idea_negocio'] = df['idea_negocio'].str.replace("-"," ")
+    df['idea_negocio'] = df['idea_negocio'].str.replace("_"," ")
+
+
+    df['barrio'] = df['barrio'].str.lower()
+    df['barrio'] = df['barrio'].str.replace("-"," ")
+    df['barrio'] = df['barrio'].str.replace("_"," ")
+
+
+    df['estrato'] = df['estrato'].astype(int)
+
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
+
+    df['fecha_de_beneficio']= pd.to_datetime(df['fecha_de_beneficio'],
+                                            dayfirst= True,
+                                            format='mixed')
+
+    df['monto_del_credito'] = df['monto_del_credito'].str.strip("$")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(" ","")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(".00","")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(",","")
+
+    df['línea_credito'] = df['línea_credito'].str.lower()
+    df['línea_credito'] = df['línea_credito'].str.replace("-"," ")
+    df['línea_credito'] = df['línea_credito'].str.replace("_"," ")
+
+
+    df.dropna(inplace=True)
+    df.drop_duplicates(inplace=True)
 
     return df
